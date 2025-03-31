352e6a79c842736fe06920c60d7e4874dea9715f
tracer-ysyx <tracer@ysyx.org>
2025-03-31 14:54:26 +0800

    > compile NEMU ysyx_25020086 周凯韬 Linux zhou-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:26 up 55 min,  1 user,  load average: 0.52, 0.49, 0.49

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -289,6 +289,25 @@ int32_t eval(int p, int q, bool *legal) {
     return eval(p + 1, q - 1, legal);
   }
   else {
+    // handle negative signs(maybe there will be more than one negative sign)
+    if (p <= q && tokens[p].type == TK_NEG) {
+      int32_t val = eval(p + 1, q, legal);
+      if (!*legal) return 0;
+      return -(int32_t)val;
+    }
+
+    if (p <= q && tokens[p].type == TK_DEREF) {
+      int32_t addr = eval(p + 1, q, legal);
+      if (!*legal) return 0;
+      // 检查地址是否对齐
+      if (addr & 0x3) {
+          *legal = false;
+          return 0;
+      }
+      int32_t val = vaddr_read(addr, 4);
+      return val;
+    }
+
     // find the position of operator
     int op = get_main_op(p, q);
     // printf("%d\n", op);
@@ -316,26 +335,6 @@ int32_t eval(int p, int q, bool *legal) {
       case TK_AND: return (val1 && val2) ? 1 : 0;
       default: assert(0);
     }
-    
-    // handle negative signs(maybe there will be more than one negative sign)
-    while (p <= q && tokens[p].type == TK_NEG) {
-      int32_t val = eval(p + 1, q, legal);
-      if (!*legal) return 0;
-      return -(int32_t)val;
-    }
-
-    while (p <= q && tokens[p].type == TK_DEREF) {
-      int32_t addr = eval(p + 1, q, legal);
-      if (!*legal) return 0;
-      // 检查地址是否对齐
-      if (addr & 0x3) {
-          *legal = false;
-          return 0;
-      }
-      int32_t val = vaddr_read(addr, 4);
-      return val;
-    }
-
   }
 }
 
