865ab5ef795fc10b0b457668879bdef38bbe557b
zhou <2628735358@qq.com>
2025-03-31 20:31:24 +0800

    add cflags

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -25,6 +25,8 @@ INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
+CFLAGS += -save-temps
+
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
