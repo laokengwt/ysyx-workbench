f81dc23dc50056e1f5858ca8a54a9ad679b0f468
tracer-ysyx <tracer@ysyx.org>
2025-03-31 19:21:14 +0800

    > compile NEMU ysyx_25020086 周凯韬 Linux zhou-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:21:14 up  1:13,  1 user,  load average: 0.59, 0.35, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,7 +40,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  scan_watchpoints();
+  IFDEF(CONFIG_WATCHPOINT, scan_watchpoints();)
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
