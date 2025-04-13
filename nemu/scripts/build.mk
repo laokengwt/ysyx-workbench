e8aedfa13153e3024dd59905896e48d73e41f33b
zhou <2628735358@qq.com>
2025-04-14 01:13:27 +0800

    npc can run instruction addi

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -25,7 +25,7 @@ INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
-CFLAGS += -save-temps
+# CFLAGS += -save-temps
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include <verilated.h>
+#include "Vtop.h"
+#include "memory.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+static Vtop top;
+
+static void single_cycle() {
+  top.clk = 0; top.eval();
+  top.clk = 1; top.eval();
+}
+
+void reset(int n) {
+  top.rst = 1;
+  while (n -- > 0) single_cycle();
+  top.rst = 0;
+}
+
+int main (int argc, char** argv){
+  reset(1);
+  init_memory();
+    
+  // 测试阶段
+  for (int cycle = 0; cycle < 3; cycle++) {
+    top.inst = pmem_read(top.pc);
+    top.eval();
+    single_cycle();
+  }
+  
+  top.final();
   return 0;
 }
+
